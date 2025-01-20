5792082513e8f08d42aec9432cbb69aadcf0176c
rar9000 <clavius@Clavius.local>
2025-01-20 16:44:42 -0500

    completed

--- js/assignment.js
+++ js/assignment.js
@@ -6,3 +6,43 @@ const largestNumberElement = document.getElementById("largest-number");
 const averageNumberElement = document.getElementById("average-number");
 
 const myNumbers = [35, 130, 354, 100, 25, 134, 99, 200, 35];
+
+function findSmallestNumber(array) {
+    let smallestNumber = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < smallestNumber) { 
+            smallestNumber = array[i]; 
+            }
+        }
+        smallestNumberElement.innerText = smallestNumber;
+    }
+
+function findLargestNumber(array) {
+    let largestNumber = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] > largestNumber) { 
+            largestNumber = array[i]; 
+            }
+        }
+        largestNumberElement.innerText = largestNumber;
+    }
+
+function findAverage(array) {
+    let total = 0
+    for(let i = 0; i < array.length; i++) {
+        total += array[i];
+    }
+    let average = total / array.length;
+    averageNumberElement.innerText = average;
+}
+
+function render() {
+    findSmallestNumber(myNumbers);
+    findLargestNumber(myNumbers);
+    findAverage(myNumbers);
+}
+
+submissionBtn.addEventListener("click", function () {
+    render();
+});
+    
--- js/index.js
+++ js/index.js
@@ -1,3 +1,36 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+/*
+
+const array9000 = [4, 5, 1, 0, 9, 3, 2, 8, 6, 7]
+for (let i = 0; i < array9000.length; i++) {
+    console.log(array9000[i]);
+}
+
+let sentence = ['words', 'in', 'a', 'sentence']
+for(let words of sentence) {
+    console.log(words);
+}
+
+let words = ['w', 'o', 'r', 'd', 's']
+words.forEach(value => {
+    console.log(value); 
+});
+
+let oldSentence = ['words', 'in', 'a', 'sentence'];
+let newSentence = ['so', 'this', 'is', 'coding'];
+for (let i = 0; i < oldSentence.length; i++) {
+    oldSentence[i] = newSentence[i];
+    console.log(oldSentence);
+}
+
+let letters = ['l', 'e', 't', 't', 'e', 'r', 's']
+let srettel = ['s', 'r', 'e', 't', 't', 'e', 'l']
+letters.forEach((value, index) => {
+    letters[index] = srettel[index];
+});
+console.log(letters);
+
+*/
